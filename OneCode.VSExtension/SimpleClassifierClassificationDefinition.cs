ffc7b0ed058e64af3898a7fb4c0858550eea70a8
Konstantin S <havendv@gmail.com>
2019-12-02 20:15:23 +1000

    Added SimpleClassifier

---
+++ OneCode.VSExtension/SimpleClassifierClassificationDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace OneCode.VsExtension
+{
+    /// <summary>
+    /// Classification type definition export for SimpleClassifier
+    /// </summary>
+    internal static class SimpleClassifierClassificationDefinition
+    {
+        // This disables "The field is never used" compiler's warning. Justification: the field is used by MEF.
+#pragma warning disable 169
+
+        /// <summary>
+        /// Defines the "SimpleClassifier" classification type.
+        /// </summary>
+        [Export(typeof(ClassificationTypeDefinition))]
+        [Name("SimpleClassifier")]
+        private static ClassificationTypeDefinition typeDefinition;
+
+#pragma warning restore 169
+    }
+}
---
+++ OneCode.VSExtension/SimpleClassifierFormat.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace OneCode.VsExtension
+{
+    /// <summary>
+    /// Defines an editor format for the SimpleClassifier type that has a purple background
+    /// and is underlined.
+    /// </summary>
+    [Export(typeof(EditorFormatDefinition))]
+    [ClassificationType(ClassificationTypeNames = "SimpleClassifier")]
+    [Name("SimpleClassifier")]
+    [UserVisible(true)] // This should be visible to the end user
+    [Order(Before = Priority.Default)] // Set the priority to be after the default classifiers
+    internal sealed class SimpleClassifierFormat : ClassificationFormatDefinition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleClassifierFormat"/> class.
+        /// </summary>
+        public SimpleClassifierFormat()
+        {
+            this.DisplayName = "SimpleClassifier"; // Human readable version of the name
+            this.BackgroundColor = Colors.BlueViolet;
+            this.TextDecorations = System.Windows.TextDecorations.Underline;
+        }
+    }
+}
