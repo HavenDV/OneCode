c76bc24a4ea6d412fd1626c9ec904ece5620e5d4
Konstantin S <havendv@gmail.com>
2019-12-04 18:13:13 +1000

    Refactored

--- OneCode.VSExtension/UI/Windows/OneCodeWindow.cs
+++ OneCode.VSExtension/UI/Windows/OneCodeWindow.cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
-using OneCode.VsExtension.UI.Controls;
 
 namespace OneCode.VsExtension.UI.Windows
 {
@@ -28,7 +27,7 @@ namespace OneCode.VsExtension.UI.Windows
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
-            Content = new OneCodeWindowControl();
+            Content = new Controls.OneCodeWindowControl();
         }
 
         public OneCodeWindow(string _) : this()
